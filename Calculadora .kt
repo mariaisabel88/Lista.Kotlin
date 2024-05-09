8d3dda89781c06eb09d346165e0cd8886f29cd10
Maria Isabel <166646693+mariaisabel88@users.noreply.github.com>
2024-05-09 18:02:41 -0300

    Calculadora .kt

---
+++ Calculadora .kt
@@ -0,0 +1,18 @@
+class Calculadora {
+    fun somar(a: Int, b: Int){
+        return a + b 
+    }
+    
+    fun subtrair(a: int, b: int): int 
+        return a - b 
+   }
+    
+      fun multiplicar(a: Int, b: Int): Int {
+          return a * b 
+      }
+       
+      fun dividir(a Int, b Int): Int { 
+          return a / b 
+      }
+      
+}
